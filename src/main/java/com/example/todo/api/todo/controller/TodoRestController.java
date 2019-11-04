4afe7e832759aaca32147fd27513443da0ae7f5a
HironoriSato <maza.hs5360@gmail.com>
2019-11-04 18:40:23 +0900

    [add]postTodosメソッドを新規作成

--- src/main/java/com/example/todo/api/todo/controller/TodoRestController.java
+++ src/main/java/com/example/todo/api/todo/controller/TodoRestController.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +38,13 @@ public class TodoRestController {
             todoResources.add(beanMapper.map(todo, TodoResource.class));
         }
         return todoResources;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // HTTPステータスとして、”201 Created”を設定
+    public TodoResource postTodos(@RequestBody @Validated TodoResource todoResource) {
+        Todo createdTodo = todoService.create(beanMapper.map(todoResource, Todo.class));
+        TodoResource createdTodoResponse = beanMapper.map(createdTodo, TodoResource.class);
+        return createdTodoResponse; // spring-mvc-rest.xmlに定義したMappingJackson2HttpMessageConverterによってJSONにシリアライズし返却
     }
 }
