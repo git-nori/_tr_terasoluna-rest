5e03881243624477c370704b514a358a2185af24
HironoriSato <maza.hs5360@gmail.com>
2019-11-04 19:04:46 +0900

    [add]putTodoメソッドを新規作成

--- src/main/java/com/example/todo/api/todo/controller/TodoRestController.java
+++ src/main/java/com/example/todo/api/todo/controller/TodoRestController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -54,6 +55,14 @@ public class TodoRestController {
     public TodoResource getTodo(@PathVariable("todoId") String todoId) {
         Todo todo = todoService.findOne(todoId);
         TodoResource todoResource = beanMapper.map(todo, TodoResource.class);
+        return todoResource;
+    }
+
+    @PutMapping("/{todoId}")
+    @ResponseStatus(HttpStatus.OK)
+    public TodoResource putTodo(@PathVariable("todoId") String todoId) {
+        Todo finishedTodo = todoService.finish(todoId);
+        TodoResource todoResource = beanMapper.map(finishedTodo, TodoResource.class);
         return todoResource;
     }
 }
